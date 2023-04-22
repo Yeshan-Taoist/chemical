f819dfcecc92ced991bb26b73cfc598b603fe0a8
Yeshan-Taoist <Yeshan-Taoist@users.noreply.github.com>
2023-04-22 04:12:06 +0000

    deploy: db3b24f3f70d2f503ef0c3b07245153f4e07a0a4

--- assets/chunks/@localSearchIndexroot.a4e1dc09.js
+++
@@ -1,1 +0,0 @@
-const e='{"documentCount":1,"nextId":1,"documentIds":{"0":"/chemical/#hello-world"},"fieldIds":{"title":0,"titles":1,"text":2},"fieldLength":{"0":[2,1,1]},"averageFieldLength":[2,1,1],"storedFields":{"0":{"title":"Hello World","titles":[]}},"dirtCount":0,"index":[["world",{"0":{"0":1}}],["hello",{"0":{"0":1}}]],"serializationVersion":2}';export{e as default};
